ecb498d4c4cda3db11a2790cc027647cb001faa9
TacticalDan <gorksorf@gmail.com>
2013-12-27 22:01:35 -0700

    Sprite associated with Unit on client side

---
+++ js/Graphics.js
@@ -0,0 +1,17 @@
+var Graphics = {};
+
+Graphics.init = function (){
+	// create an new instance of a pixi stage
+	Graphics.stage = new PIXI.Stage(0x66FF99);
+	// create a renderer instance
+	Graphics.renderer = PIXI.autoDetectRenderer(1024, 768);
+	// add the renderer view element to the DOM
+	document.body.appendChild(Graphics.renderer.view);
+	// create a texture from an image path
+	Unit.texture = PIXI.Texture.fromImage("img/bunny.png");
+	Graphics.isInitialized = true;
+}
+
+Graphics.update = function(){
+	Graphics.renderer.render(Graphics.stage);
+}

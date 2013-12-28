a0259e1f88657c3fc18726566b0b18c2b486969a
Nathan <nathan@ubuntu.(none)>
2013-12-27 18:36:01 -0700

    Bunny command is operational

--- js/Point.js
+++ js/Point.js
@@ -1,26 +1,39 @@
 function Point (x, y){
 	this.x = x || 0;
 	this.y = y || 0;
-}
 
-Point.add = function(point){
-	this.x += point.x;
-	this.y += point.y;
-}
-
-Point.sub = function(point){
-	this.x -= point.x;
-	this.y -= point.y;
-}
-
-Point.normalize = function(){
-	var length = this.magnitude;
-	if (length > 0){
-		this.x /= length;
-		this.y /= length;
+	this.init = function(point){
+		this.x = point.x;
+		this.y = point.y;
+		return this;
 	}
-}
 
-Point.magnitude = function(){
-	return Math.sqrt(x*x+y*y);
+	this.add = function(point){
+		this.x += point.x;
+		this.y += point.y;
+		return this;
+	}
+
+	this.sub = function(point){
+		this.x -= point.x;
+		this.y -= point.y;
+		return this;
+	}
+
+	this.normalize = function(){
+		var length = this.magnitude();
+		if (length > 0){
+			this.x /= length;
+			this.y /= length;
+		}
+		return this;
+	}
+
+	this.magnitude = function(){
+		return Math.sqrt(this.x*this.x+this.y*this.y);
+	}
+
+	this.getInfo = function(){
+		return "Point: {"+this.x +", "+ this.y+"}";
+	}
 }

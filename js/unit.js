ae1d7d56037bb37f4b6091d178f525ef5938abb0
Ben Franck <benjamin.franck@gmail.com>
2013-12-27 18:37:54 -0700

    Delete unit.js

--- js/unit.js
+++
@@ -1,8 +0,0 @@
-function Unit (id, position, goal) {
-    this.id = id;
-    this.position = position;
-    this.goal = goal;
-    this.getInfo = function() {
-        return id+" "+position +" "+goal;
-    };
-}

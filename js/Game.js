a996a6c8c1d24ba92a29b3381f002134b1799145
Nathan <nathan@ubuntu.(none)>
2013-12-27 19:46:58 -0700

    Some code refactoring

--- js/Game.js
+++ js/Game.js
@@ -7,28 +7,12 @@ Game.createNewPlayer = function(){ //go through player list and create new playe
 
 }
 
-Game.createNewUnit = function(){ //create new unit
-	var created = false;
-	var i;	
-	var unit = new Unit(i, new Point(20, 20), new Point(200, 200));
-	console.log(unit.position.getInfo());
-	//loop through existing list
-	for (i = 0; i < this.units.length; i++){
-		if (this.units[i] == null){
-			this.units[i] = unit;
-			created = true;
-		}
-	}
-	//or tack onto end of list
-	if (!created) this.units.push(unit);
-}
-
 Game.update = function(){ //simulate game for one tick
 	Time.update();
 	var i;
 
 	//update all the units
-	for (i = 0; i < this.units.length; i++){
-		this.units[i].update();
+	for (i = 0; i < Unit.list.length; i++){
+		Unit.list[i].update();
 	}
 }

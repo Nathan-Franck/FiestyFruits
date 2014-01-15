917590d5e60cadd8d3a1acfb0ffe948ec07bb893
TacticalDan <gorksorf@gmail.com>
2014-01-14 17:12:52 -0800

    Differentiate left and right clicks to do both selecting and commanding in the future

--- js/Input.js
+++ js/Input.js
@@ -1,9 +1,29 @@
 var Input = {}
 
+Input.leftClick = function(e){
+	
+}
+
+Input.middleClick = function(e){
+
+}
+
+Input.rightClick = function(e){
+	connection.socket.emit('commandUnits', connection.player.commandUnits({goal:{x:e.pageX, y:e.pageY}}));
+}
+
 Input.registerEvents = function(connection){
     $(window).mousedown(function(e) {
-		connection.socket.emit('commandUnits', connection.player.commandUnits({goal:{x:e.pageX, y:e.pageY}}));
+    	switch (e.which){
+    		case 1: Input.leftClick(e); break;
+    		case 2: Input.middleClick(e); break;
+    		case 3: Input.rightClick(e); break;
+    	}
+		
 	});
+	window.oncontextmenu = function() {
+	    return false;
+	};
 }
 
 Game.classList.push(Input);
